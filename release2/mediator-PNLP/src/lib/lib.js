4cef283923ec9eff4d3c635a10578cbe9996537b
gerard-bisama <gerbis2000@yahoo.fr>
2022-03-30 05:16:20 +0000

    deactivated break in the lib

--- release2/mediator-PNLP/src/lib/lib.js
+++ release2/mediator-PNLP/src/lib/lib.js
@@ -1314,7 +1314,7 @@ exports.buildADXPayloadFromRequisitionsList=function(requisitionObjectsList,meta
 	let compteur=0;
 	for(let requisitionObject of requisitionObjectsList){
 		compteur++;
-		if(compteur==2) break;
+		//if(compteur==2) break;
 		let validPeriodReported=requisitionObject.startDate;
 		
 		let groupObject= {group:[{_attr:{orgUnit:requisitionObject.location,period:validPeriodReported+"/P1M",completeDate:currentZFormatDate}},
